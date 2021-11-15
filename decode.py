597f7a8660712807cf72ff115ce0e2a95dfae665
SaumyaBhushan <saumya@knoldus.com>
2021-11-15 13:11:03 +0530

    deleted from main branch

--- decode.py
+++
@@ -1,9 +0,0 @@
-import cv2
-# Create The Decoder
-decoder = cv2.QRCodeDetector()
-# Load Your Data
-file_name = "QR.png"
-image = cv2.imread(file_name)
-# Decode and Print the required information
-link, data_points, straight_qrcode = decoder.detectAndDecode(image)
-print(link)
